9bd76ecffdd1a200990d1de4eb2d31c5b3e6bd05
Jpriem <jpriem@sogyo.nl>
2024-11-29 13:58:14 +0100

    Finished day 7

---
+++ 2022/Day07_1/ElfDirectory.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Xml.Linq;
+
+namespace Day07_1
+{
+    public class ElfDirectory
+    {
+        private string name;
+        private ElfDirectory? parentDirectory;
+        private List<ElfDirectory> childDirectories = new List<ElfDirectory>();
+        private List<double> fileSizes = new List<double>();
+
+        public ElfDirectory(ElfDirectory? parentDirectory, string name)
+        {
+            this.name = name;
+            this.parentDirectory = parentDirectory;
+        }
+
+        public void AddFile(double fileSize)
+        {
+            this.fileSizes.Add(fileSize);
+        }
+
+
+        public void AddDirectory(ElfDirectory directory)
+        {
+            this.childDirectories.Add(directory);
+        }
+
+        public ElfDirectory GetParentDirectory()
+        {
+            return this.parentDirectory ?? this;
+        }
+
+        public ElfDirectory GetChildDirectory(string name)
+        {
+            return this.childDirectories.FirstOrDefault(dir => dir.name == name) ?? this;
+        }
+
+        public double GetTotalFileSize()
+        {
+            double total = this.GetCurrentDirectoryFileSize();
+
+            foreach (ElfDirectory directory in this.childDirectories)
+            {
+                total += directory.GetTotalFileSize();
+            }
+
+            return total;
+        }
+
+        private double GetCurrentDirectoryFileSize()
+        {
+            return this.fileSizes.Sum();
+        }
+
+        private bool HasChildDirectories()
+        {
+            return this.childDirectories.Count > 0;
+        }
+
+        public IEnumerable<ElfDirectory> GetAllDescendants()
+        {
+            foreach (var childDirectory in this.childDirectories)
+            {
+                yield return childDirectory;
+                             
+                if (childDirectory.HasChildDirectories())
+                {
+                    foreach (var grandChildDirectory in childDirectory.GetAllDescendants())
+                    {
+                        yield return grandChildDirectory;
+                    }
+                }
+            }
+        }
+
+        public ElfDirectory GetRootDirectory()
+        {
+            if( this.parentDirectory == null)
+            {
+                return this;
+            }
+
+            return this.parentDirectory.GetRootDirectory();
+        }
+
+    }
+}
---
+++ 2022/Day07_1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using Day07_1;
+double totalFileSize = 0;
+string line;
+string lastCommand = "";
+
+string newDirName = "";
+ElfDirectory currentDir = null;
+while ((line = Console.ReadLine()) != null)
+{
+    string[] splittedLine = line.Split(' ');
+    if (splittedLine[0] == "$")
+    {
+        string currentCommand = splittedLine[1];
+        
+        if (currentCommand == "cd")
+        {
+            if (splittedLine[2] == "..")
+            {
+                currentDir = currentDir.GetParentDirectory();
+            }
+            else
+            {
+                newDirName = splittedLine[2];
+
+                if (currentDir == null)
+                {
+                    currentDir = new ElfDirectory(null, newDirName);
+                }
+                else
+                {
+                    var newDirectory = new ElfDirectory(currentDir, newDirName);
+                    currentDir.AddDirectory(newDirectory);
+                    currentDir = newDirectory;
+                }
+            }
+        }
+
+        lastCommand = currentCommand;
+    }
+    else
+    {
+        if (lastCommand == "ls")
+        {
+            if (splittedLine[0] != "dir")
+            {
+                currentDir.AddFile(double.Parse(splittedLine[0]));
+
+            }
+        }
+    }
+}
+var rootDir = currentDir.GetRootDirectory();
+
+foreach(ElfDirectory dir in rootDir.GetAllDescendants())
+{
+    double fileSize = dir.GetTotalFileSize();
+
+    if (fileSize <= 100000)
+    {
+        totalFileSize += fileSize;
+    }
+
+}
+
+Console.WriteLine(totalFileSize);
+await Task.Delay(5000);
